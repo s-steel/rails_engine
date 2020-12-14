867d8bd2e831005c76fe56fa98a1283a629b4514
Sean Steel <seanrsteel@gmail.com>
2020-12-14 14:48:43 -0700

    Create a merchant/id/items response with testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants
       resources :items
+      scope :merchants do
+        get '/:id/items', to: 'merchants/items#index', as: :merchant_items
+      end
     end
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::Company.name }
-  end
 
-  trait :with_items do
-    after :create do |merchant|
-      create_list(:item, 5)
+    trait :with_items do
+      after :create do |merchant|
+        create_list(:item, 5, merchant: merchant)
+      end
     end
   end
 end
